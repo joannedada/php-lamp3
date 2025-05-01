eed746fd78a44f6e9a436436f539b39f9f93e6fb
Joanne Obodoagwu <143835895+joannedada@users.noreply.github.com>
2025-05-01 16:16:21 +0100

    Update db.php

--- php/db.php
+++ php/db.php
@@ -4,7 +4,7 @@ function Createdb(){
     $servername = "localhost";
     $username = "root";
     $password = "Admin123*";
-    $dbname = "Meionnabookstore";
+    $dbname = "bookstore";
 
     // create connection
     $con = mysqli_connect($servername, $username, $password);
